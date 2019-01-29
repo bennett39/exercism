d8880a82eb72c7c0f29deb5160a1a0739dcfde9f
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-01-28 19:53:12 -0500

    Simplify to one line

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,6 +1,1 @@
-export const isLeap = (year) => {
-  if ((year % 4 === 0 && year % 100 != 0) || year % 400 === 0) {
-    return true;
-  }
-  return false;
-}
+export const isLeap = year => year % 4 === 0 && (year % 100 != 0 || year % 400 === 0)
