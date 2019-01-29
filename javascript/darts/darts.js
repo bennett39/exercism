1e93c4fe1657ffd659a84d0d2ade52e8bf766a60
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-01-28 20:03:10 -0500

    Refactor using const and arrows, return on same line

--- javascript/darts/darts.js
+++ javascript/darts/darts.js
@@ -1,16 +1,11 @@
-function calcDistCenter(x, y) {
-  return Math.sqrt((x * x) + (y * y));
-}
+const calcDistCenter = (x, y) => Math.sqrt(x * x + y * y);
 
-function givePointsByDist(distance) {
-  if (distance <= 1) { return 10; }
-  else if (1 < distance && distance <= 5) { return 5; }
-  else if (5 < distance && distance <= 10) { return 1; }
-  else if (distance > 10) { return 0; }
+const givePointsByDist = distance => {
+  if (distance <= 1)  return 10;
+  else if (1 < distance && distance <= 5) return 5;
+  else if (5 < distance && distance <= 10) return 1;
+  else if (distance > 10) return 0;
   return null;
 }
 
-export const solve = (x, y) => {
-  const distance = calcDistCenter(x, y);
-  return givePointsByDist(distance);
-}
+export const solve = (x, y) => givePointsByDist(calcDistCenter(x, y));
