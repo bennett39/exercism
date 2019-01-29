c3b3e2a8c6bbd04d2b395ee1812d3bc5bfcf56c2
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-01-28 20:55:18 -0500

    Use .split('').reverse().join('')

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -1,5 +1,1 @@
-export const reverseString = string => {
-  let reversed = '';
-  for (let i = string.length - 1; i >= 0; i--) reversed += string[i];
-  return reversed;
-}
+export const reverseString = string => string.split('').reverse().join('');
