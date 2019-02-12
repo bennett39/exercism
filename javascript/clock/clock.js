6fbc3cae0cde6d3cc138945ad83cd7f207bc996d
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-02-11 21:09:39 -0500

    Define Clock as new class

--- javascript/clock/clock.js
+++ javascript/clock/clock.js
@@ -1,25 +1,25 @@
-export const at = (hour, minute) => {
-  const time = rollover(handleFalsy(hour), handleFalsy(minute));
-  hour = time.hour;
-  minute = time.minute;
-
-  function handleFalsy(digit) { 
-    return (digit) ? digit : 0 
-  }
-
-  function rollover(hour, minute) { 
-    hour = (hour + Math.floor(minute/60)) % 24;
-    minute %= 60;
-    if (minute < 0) minute += 60;
-    if (hour < 0) hour += 24;
-    return {hour: hour, minute: minute}
-  }
-
-  function prependZero(digit) {
-    return (digit < 10) ? '0' + String(digit) :  String(digit);
-  }  
-
-  return prependZero(hour) + ':' + prependZero(minute);
+function rollover(hour, minute) { 
+  hour = (hour + Math.floor(minute/60)) % 24;
+  minute %= 60;
+  if (minute < 0) minute += 60;
+  if (hour < 0) hour += 24;
+  return {hour: hour, minute: minute}
 }
 
+function prependZero(digit) {
+  return (digit < 10) ? '0' + String(digit) :  String(digit);
+}  
 
+class Clock {
+  constructor (hour, minute) {
+    const time = rollover(hour, minute);
+    this.hour = time.hour;
+    this.minute = time.minute;
+  }
+
+  toString() {
+    return prependZero(this.hour) + ':' + prependZero(this.minute);
+  }
+}
+
+export const at = (hour, minute = 0) => new Clock(hour, minute);
