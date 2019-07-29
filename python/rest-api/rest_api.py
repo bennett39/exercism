03c6e7b65bf6f2b2e0163b6adc8b48a92c4ba089
Bennett <34491412+bennett39@users.noreply.github.com>
2019-07-29 13:12:02 -0400

    Refactor code

--- python/rest-api/rest_api.py
+++ python/rest-api/rest_api.py
@@ -20,7 +20,7 @@ class RestAPI(object):
         if url == '/add':
             try:
                 username = payload["user"]
-            except:
+            except IndexError:
                 raise IndexError("No username provided")
             new_user_record = {
                 "name": username,
@@ -29,40 +29,35 @@ class RestAPI(object):
                 "balance": 0
             }
             self.database["users"].append(new_user_record)
-            response = self.database["users"][-1]
+            response = json.dumps(self.database["users"][-1])
         if url == '/iou':
             try:
-                lender = payload["lender"]
-                borrower = payload["borrower"]
+                lender, borrower = payload["lender"], payload["borrower"]
                 leant = borrowed = payload["amount"]
-            except:
-                raise IndexError("Provide lender, borrower, and amount")
+            except IndexError:
+                return json.dumps({'error': 'Provide lender, borrower, and amount'})
             for user in self.database["users"]:
                 if user["name"] == lender:
                     user["balance"] += leant
                     if borrower in user["owes"]:
-                        leant = user["owes"][borrower] - leant
-                        if leant > 0:
-                            user["owes"][borrower] = leant
+                        leant = leant - user["owes"][borrower]
+                        if leant < 0:
+                            user["owes"][borrower] = abs(leant)
                         else:
                             del user["owes"][borrower]
                     if borrower not in user["owes"] and leant != 0:
-                        leant = abs(leant)
                         user["owed_by"].setdefault(borrower, 0)
                         user["owed_by"][borrower] += leant
                 if user["name"] == borrower:
                     user["balance"] -= borrowed
                     if lender in user["owed_by"]:
-                        borrowed = user["owed_by"][lender] - borrowed
-                        if borrowed > 0:
-                            user["owed_by"][lender] = borrowed
+                        borrowed = borrowed - user["owed_by"][lender]
+                        if borrowed < 0:
+                            user["owed_by"][lender] = abs(borrowed)
                         else:
                             del user["owed_by"][lender]
                     if lender not in user["owed_by"] and borrowed != 0:
-                        borrowed = abs(borrowed)
                         user["owes"].setdefault(lender, 0)
                         user["owes"][lender] += borrowed
-            response = json.loads(self.get('/users', json.dumps({
-                'users': [lender, borrower]
-            })))
-        return json.dumps(response)
+            response = self.get('/users', json.dumps({'users': [lender, borrower]}))
+        return response
