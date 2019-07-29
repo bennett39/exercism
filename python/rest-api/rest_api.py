e9310f3e5c1c6b5bdb7cc3cf61672a8c983e5cfe
Bennett <34491412+bennett39@users.noreply.github.com>
2019-07-29 13:01:50 -0400

    Working solution - all tests pass

--- python/rest-api/rest_api.py
+++ python/rest-api/rest_api.py
@@ -34,18 +34,34 @@ class RestAPI(object):
             try:
                 lender = payload["lender"]
                 borrower = payload["borrower"]
-                amount = payload["amount"]
+                leant = borrowed = payload["amount"]
             except:
                 raise IndexError("Provide lender, borrower, and amount")
             for user in self.database["users"]:
                 if user["name"] == lender:
-                    user["owed_by"].setdefault(borrower, 0)
-                    user["owed_by"][borrower] += amount
-                    user["balance"] += amount
+                    user["balance"] += leant
+                    if borrower in user["owes"]:
+                        leant = user["owes"][borrower] - leant
+                        if leant > 0:
+                            user["owes"][borrower] = leant
+                        else:
+                            del user["owes"][borrower]
+                    if borrower not in user["owes"] and leant != 0:
+                        leant = abs(leant)
+                        user["owed_by"].setdefault(borrower, 0)
+                        user["owed_by"][borrower] += leant
                 if user["name"] == borrower:
-                    user["owes"].setdefault(lender, 0)
-                    user["owes"][lender] += amount
-                    user["balance"] -= amount
+                    user["balance"] -= borrowed
+                    if lender in user["owed_by"]:
+                        borrowed = user["owed_by"][lender] - borrowed
+                        if borrowed > 0:
+                            user["owed_by"][lender] = borrowed
+                        else:
+                            del user["owed_by"][lender]
+                    if lender not in user["owed_by"] and borrowed != 0:
+                        borrowed = abs(borrowed)
+                        user["owes"].setdefault(lender, 0)
+                        user["owes"][lender] += borrowed
             response = json.loads(self.get('/users', json.dumps({
                 'users': [lender, borrower]
             })))
